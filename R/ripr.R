11baa393aa96c2c58867fc48d08abd470f905e41
Per Unneberg <per.unneberg@scilifelab.se>
2020-04-17 19:10:10 +0200

    Add main package R file and reference sequence

---
+++ R/ripr.R
@@ -0,0 +1,19 @@
+#' ripr: A package for calculating rip statistics
+#'
+#' The ripr package provides functionality for calculating
+#' repeat-induced point mutation (rip) statistics from sequence data.
+#' The package has function for reading repeatmasker output
+#'
+#'
+#' @section  functions:
+#'
+#' plotRip
+#'
+#' @docType package
+#' @name ripr
+#'
+#' @import methods
+#'
+NULL
+
+
