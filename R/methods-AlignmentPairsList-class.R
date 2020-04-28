ad516fd2aa2bf809079dbb84d0a48baccdeb5668
Per Unneberg <per.unneberg@scilifelab.se>
2020-04-28 11:31:09 +0200

    Use dplyr::bind_rows to combine AlignmentPairs objects

--- R/methods-AlignmentPairsList-class.R
+++ R/methods-AlignmentPairsList-class.R
@@ -15,29 +15,48 @@ setMethod("AlignmentPairsList", "list",
 ##'
 ##' @param x AlignmentPairsList object
 ##' @param ... additional parameters to lapply
+##' @param .id name of id column added by dplyr::bind_rows
 ##'
 ##' @return data.frame
+##'
+##' @importFrom dplyr bind_rows
 ##'
 ##' @export
 ##'
 setMethod("as.data.frame", signature = "AlignmentPairsList",
-          function(x, ...) {
-    do.call("rbind", lapply(x, as.data.frame, ...))
+          function(x, ..., .id="id") {
+    dplyr::bind_rows(lapply(x, as.data.frame, ...), .id=.id)
 })
 
 
 
-##' @importFrom ggplot2 autoplot ggplot geom_point geom_boxplot geom_violin
-autoplot.AlignmentPairsList <- function(object, aes, which="point", ...) {
-    data <- do.call("rbind", lapply(object, as.data.frame))
+##' autoplot.AlignmentPairsList
+##'
+##' @importFrom ggplot2 autoplot ggplot geom_point geom_boxplot geom_violin geom_density
+##'
+##' @param object AlignmentPairsList
+##' @param aes aes mapping
+##' @param vars variable mapping to facet plots
+##' @param ... additional parameters to ggplot function
+##' @param which which plot to make. 'grid' option makes a scatter
+##'     plot with marginal densities
+##'
+##' @export
+##'
+autoplot.AlignmentPairsList <- function(object, aes, vars, ..., which="point") {
+    data <- as.data.frame(object)
     p <- ggplot(data, {{ aes }})
-    which <- match.arg(which, c("point", "boxplot", "violin"))
+    which <- match.arg(which, c("point", "boxplot", "violin", "density"))
     if (which == "point")
         p <- p + geom_point(...)
     else if (which == "boxplot")
         p <- p + geom_boxplot(...)
     else if (which == "violin")
         p <- p + geom_violin(...)
+    else if (which == "density")
+        p <- p + geom_density(...)
+    if (!missing(vars))
+        p <- p + facet_wrap( {{ vars }} )
     p
 }
 
