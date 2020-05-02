12a5345382a9a7c9d9c244aada2839f4a8049480
Per Unneberg <per.unneberg@scilifelab.se>
2020-05-02 14:16:26 +0200

    Update ripr package docs

--- R/ripr-package.R
+++ R/ripr-package.R
@@ -1,10 +1,10 @@
-#' ripr: A package for calculating RIP statistics
-#'
+#' @details
 #' The ripr package provides functionality for calculating
 #' repeat-induced point mutation (RIP) statistics from sequence data.
 #' The package has function for reading repeatmasker output,
 #' calculating RIP scores, and simple plotting functions for plotting
 #' scores along chromosomes.
+#'
 #'
 #'
 #' @section Functions:
@@ -14,13 +14,9 @@
 #'
 #' calculateRIP: calculate RIP scores from repeatmasker results
 #'
-#' plotWindows: plot scores along chromosomes
+#' autoplot: plot AlignmentItem objects or GRanges objects with scores
+#' along chromosomes
 #'
-#' @docType package
-#' @name ripr
+#' @keywords internal
 #'
-#' @import methods
-#'
-NULL
-
-
+"_PACKAGE"
